edc1befceb89f00fa073f98afcc9dc20362198ff
v-i-nay <vinay@softscrips.net>
2025-05-16 13:15:08 +0530

    added bar graphs

--- wp-site-inspector.php
+++ wp-site-inspector.php
@@ -6,10 +6,26 @@
  * Author: Your Name
  */
 
-if (!defined('ABSPATH')) exit;
+ if (!defined('ABSPATH')) exit;
 
-require_once plugin_dir_path(__FILE__) . 'admin/class-admin-ui.php';
-require_once plugin_dir_path(__FILE__) . 'admin/class-analyzer.php';
-
-// Instantiate the Admin UI class to add the admin menu & enqueue assets
-new WP_Site_Inspector_Admin_UI();
+ // Wait until all plugins are loaded before initializing
+ add_action('plugins_loaded', 'wpsi_initialize_plugin');
+ 
+ function wpsi_initialize_plugin() {
+     // Check if the class already exists (possibly from another similar plugin)
+     if (!class_exists('WP_Site_Inspector_Admin_UI')) {
+            // Load required classes safely
+     require_once plugin_dir_path(__FILE__) . 'admin/class-admin-ui.php';
+     require_once plugin_dir_path(__FILE__) . 'admin/class-analyzer.php';
+ 
+     // Initialize your plugin
+     new WP_Site_Inspector_Admin_UI();
+     }else{
+         add_action('admin_notices', function () {
+             echo '<div class="notice notice-warning is-dismissible">';
+             echo '<p><strong>WP Site Inspector:</strong> Another plugin with similar functionality is already active. Please deactivate the other version to use the Current one.</p>';
+             echo '</div>';
+         });
+         return;
+     } 
+ }
